26c8bf11da2c17e52736bf8a16cf820d2e07a410
Oswaldo <oswaldodomingo@gmail.com>
2026-05-05 00:01:03 +0200

    añadir template de página en construcción

---
+++ web/templates/construccion.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Página en Construcción</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div style="text-align: center; margin-top: 50px;">
+        <h1>🚧 Página en Construcción 🚧</h1>
+        <p>Esta sección se encuentra actualmente en desarrollo. Volveremos pronto.</p>
+        <a href="index.php?ctl=home">Volver al Inicio</a>
+    </div>
+</body>
+</html>
