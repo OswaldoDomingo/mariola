22b468a754bf1bf6cfadf14b524c7eb080bf9bba
Oswaldo <oswaldodomingo@gmail.com>
2026-05-18 10:52:56 +0200

    refactor(menu): simplificar navegación de administrador

--- web/templates/menuAdmin.php
+++ web/templates/menuAdmin.php
@@ -3,21 +3,11 @@
 		<li class="nav-item ">
 			<a href="index.php?ctl=home" class="nav-link text-white">INICIO</a>
 		</li>
-        <li class="nav-item ">
-			<a href="index.php?ctl=insertarAlumno" class="nav-link text-white">Insertar alumno</a>
-		</li>
-        <li class="nav-item ">
-			<a href="index.php?ctl=elimninarAlumno" class="nav-link text-white">Eliminar alumno</a>
-		</li>
-        <li class="nav-item ">
-			<a href="index.php?ctl=modificarAlumno" class="nav-link text-white">Modificar alumno</a>
-		</li>
-        <li class="nav-item ">
-			<a href="index.php?ctl=buscarAlumno" class="nav-link text-white">Buscar alumno</a>
-		</li>
 		<li class="nav-item ">
 			<a href="index.php?ctl=salir" class="nav-link text-white">CERRAR SESION</a>
 		</li>
 	</ul>
 </div>
-<div class="text-white p-2">Bienvenido Admin <?php echo $_SESSION['usuario']?></div>
+<div class="text-white p-2">Bienvenido Admin <?php echo $_SESSION[
+    "usuario"
+]; ?></div>
