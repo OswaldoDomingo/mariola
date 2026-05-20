2f9bf3ee58cafec702d3720e92b94f136f82b786
Oswaldo <oswaldodomingo@gmail.com>
2026-05-20 20:18:22 +0200

    ui(navegacion): limpieza de menús y mejora del panel admin
    - Se centra el contenido y los botones en la página inicioadm.php.
    - Se eliminan del menú superior del administrador los enlaces directos (Insertar, Modificar, etc.) para centralizarlos en el panel.
    - Se ocultan temporalmente 'LETRAS' y 'OTROS' del menú de usuario normal.

--- web/templates/menuAdmin.php
+++ web/templates/menuAdmin.php
@@ -1,13 +1,11 @@
-<div class="collapse navbar-collapse" id="navbarSupportedContent">
-	<ul class="navbar-nav ms-auto text-white">
-		<li class="nav-item ">
-			<a href="index.php?ctl=home" class="nav-link text-white">INICIO</a>
-		</li>
-		<li class="nav-item ">
-			<a href="index.php?ctl=salir" class="nav-link text-white">CERRAR SESION</a>
-		</li>
-	</ul>
-</div>
+<ul class="navbar-nav ms-auto text-white">
+	<li class="nav-item ">
+		<a href="index.php?ctl=home" class="nav-link text-white">INICIO</a>
+	</li>
+	<li class="nav-item ">
+		<a href="index.php?ctl=salir" class="nav-link text-white">CERRAR SESION</a>
+	</li>
+</ul>
 <div class="text-white p-2">Bienvenido Admin <?php echo $_SESSION[
     "usuario"
 ]; ?></div>
