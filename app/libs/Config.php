2d9ca94f8cd85f1fc0f2c37301f9b034f13c800a
Oswaldo <oswaldodomingo@gmail.com>
2026-06-05 21:44:13 +0200

    Eliminando hojas css que no se usan

--- app/libs/Config.php
+++ app/libs/Config.php
@@ -6,7 +6,7 @@ class Config {
     public static $mvc_bd_usuario = "mariola";
     public static $mvc_bd_clave = "mariola";
     // public static $mvc_vis_css = "style.css";
-    public static $mvc_vis_css = ["style.css", "estillogin.css", "stylephp.css", "css-modificado.css", "ESTILO.css", "ESTILOmario.css"];
+    public static $mvc_vis_css = ["style.css", "css-modificado.css"];
     public static function cargarCss() {
         foreach (self::$mvc_vis_css as $cssFile) {
             echo '<link rel="stylesheet" type="text/css" href="css/' . $cssFile . '">' . "\n";
